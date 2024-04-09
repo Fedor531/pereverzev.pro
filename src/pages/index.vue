2aceda497cd5c05d95c51fac823980cbb2bf92fc
Fedor Pereverzev <fedor531@yandex.ru>
2024-04-09 18:55:06 +0700

    fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,8 @@
 <template>
   <h1>
     pereverzev.pro !!!
-    {{ x }}
   </h1>
 </template>
 
 <script setup lang="ts">
-console.log('TEST')
-const x = ref<boolean>(false);
 </script>
-rsync --archive --compress --delete . root@62.109.4.55:/var/www/pereverzev.pro/nuxt/
