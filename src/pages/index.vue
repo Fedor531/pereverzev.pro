7b0830e5d3627a0d872573945ec5c68fc0cac8c8
Fedor Pereverzev <fedor531@yandex.ru>
2024-04-26 13:25:59 +0700

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <h1>
-    Тестовый текст
-  </h1>
+  <div class="fedya_test">
+    <input type="file" accept="application/pdf">
+  </div>
 </template>
+
+
+<script lang="ts" setup>
+
+onMounted(() => {
+  const inp = document.querySelector( "input" );
+  inp.onchange = e => {
+    const file = inp.files[0];
+    displayRenamedPDF(file, '[forced]' + file.name)
+      .then(console.log);
+  }
+
+  async function displayRenamedPDF(file, filename) {
+    // we use an hard-coded fake path
+    // to not interfere with legit requests
+    const reg_path = "/caches/";
+    const url = reg_path + filename;
+    const store = await caches.open( "name-forcer" );
+    await store.put( url, new Response( file ) );
+    const frame = document.createElement( "iframe" );
+    frame.width = 400
+    frame.height = 500;
+    const test = document.querySelector('.fedya_test');
+    test.append( frame );
+    frame.src = url;
+  }
+// clear previously cached files
+//   (async () => {
+//     const store = await caches.open( "name-forcer" );
+//     const keys = await store.keys();
+//     for( const req of keys ) {
+//       store.delete( req );
+//     }
+//   })();
+})
+</script>
