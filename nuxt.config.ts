99498d360a78e7da8486379d4254854f4bad8d6e
Fedor Pereverzev <fedor531@yandex.ru>
2024-06-10 10:55:05 +0700

    feat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,61 +1,92 @@
 import path from 'path';
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: 'src',
-  app: {
-    head: {
-      htmlAttrs: { lang: 'ru' },
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1, user-scalable=no',
-      title: 'Frontend developer | Fedor Pereverzev',
-      link: [
-        {
-          rel: 'shortcut icon',
-          type: 'image/svg+xml',
-          href: '/logo.svg',
-        },
-      ],
-      meta: [
-        // Чтобы сафари не выделял номера и адреса
-        { name: 'format-detection', content: 'telephone=no' },
-        { name: 'format-detection', content: 'address=no' },
+	srcDir: 'src',
+	app: {
+		head: {
+			htmlAttrs: { lang: 'ru' },
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1, user-scalable=no',
+			title: 'Frontend developer | Fedor Pereverzev',
+			link: [
+				{
+					rel: 'shortcut icon',
+					type: 'image/svg+xml',
+					href: '/logo.svg',
+				},
+				{
+					rel: 'preload',
+					as: 'font',
+					fetchpriority: 'high',
+					href: '/assets/fonts/montserrat-v26-cyrillic_latin-regular.woff2',
+					type: 'font/woff2',
+					crossorigin: 'anonymous'
+				},
+				{
+					rel: 'preload',
+					as: 'font',
+					fetchpriority: 'high',
+					href: '/assets/fonts/montserrat-v26-cyrillic_latin-500.woff2',
+					type: 'font/woff2',
+					crossorigin: 'anonymous'
+				},
+				{
+					rel: 'preload',
+					as: 'font',
+					fetchpriority: 'high',
+					href: '/assets/fonts/montserrat-v26-cyrillic_latin-600.woff2',
+					type: 'font/woff2',
+					crossorigin: 'anonymous'
+				},
+				{
+					rel: 'preload',
+					as: 'font',
+					fetchpriority: 'high',
+					href: '/assets/fonts/montserrat-v26-cyrillic_latin-regular.woff2',
+					type: 'font/woff2',
+					crossorigin: 'anonymous'
+				},
+			],
+			meta: [
+				// Чтобы сафари не выделял номера и адреса
+				{ name: 'format-detection', content: 'telephone=no' },
+				{ name: 'format-detection', content: 'address=no' },
 
-        { name: 'msapplication-TileColor', content: '#040404' },
-        { name: 'theme-color', content: '#040404' },
-        { name: 'author', content: 'Fedor Pereverzev <fedor531@yandex.ru>' },
-        { name: 'robots', content: 'index, follow' },
-        { name: 'description', content: 'Frontend developer | Fedor Pereverzev' },
-      ],
-    },
-  },
+				{ name: 'msapplication-TileColor', content: '#040404' },
+				{ name: 'theme-color', content: '#040404' },
+				{ name: 'author', content: 'Fedor Pereverzev <fedor531@yandex.ru>' },
+				{ name: 'robots', content: 'index, follow' },
+				{ name: 'description', content: 'Frontend developer | Fedor Pereverzev' },
+			],
+		},
+	},
 
-  css: [
-    '~/assets/fonts/index.css',
-    '~/assets/styles/variables.pcss',
-    '~/assets/styles/index.pcss',
-  ],
+	css: [
+		'~/assets/fonts/index.css',
+		'~/assets/styles/variables.pcss',
+		'~/assets/styles/index.pcss',
+	],
 
-  postcss: {
-    plugins: {
-      '@csstools/postcss-global-data': {
-        files: [
-          path.resolve(__dirname, 'src/assets/styles/media-queries.pcss'),
-        ],
-      },
-      'postcss-custom-media': {},
-      'postcss-nested': {},
-      'postcss-normalize': {},
-    },
-  },
+	postcss: {
+		plugins: {
+			'@csstools/postcss-global-data': {
+				files: [
+					path.resolve(__dirname, 'src/assets/styles/media-queries.pcss'),
+				],
+			},
+			'postcss-custom-media': {},
+			'postcss-nested': {},
+			'postcss-normalize': {},
+		},
+	},
 
-  features: {
-    inlineStyles: false,
-  },
+	features: {
+		inlineStyles: false,
+	},
 
-  devtools: {
-    enabled: false,
-  },
+	devtools: {
+		enabled: false,
+	},
 });
 
 // <link rel="icon" href="/icon.svg" type="image/svg+xml">
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,38 +1,52 @@
 <template>
-  <div class="fedya_test">
-    <input type="file" accept="application/pdf">
-  </div>
+	<div class="fedya_test" style="display: flex; flex-direction: column; font-size: 40px">
+		<input type="file" accept="application/pdf">
+		<span style="font-weight: 400">
+			Федор Тест 400
+		</span>
+		<span style="font-weight: 500">
+			Федор Тест 500
+		</span>
+		<span style="font-weight: 600">
+			Федор Тест 600
+		</span>
+		<span style="font-weight: 700">
+			Федор Тест 700
+		</span>
+	</div>
 </template>
 
 
 <script lang="ts" setup>
 onMounted(async () => {
-  await navigator.serviceWorker.register('/caches.js')
+	await navigator.serviceWorker.register('/caches.js');
 
-  const inp = document.querySelector( "input" );
-  inp.onchange = e => {
-    const file = inp.files[0];
-    displayRenamedPDF(file, 'giga_test' + file.name)
-  }
-  async function displayRenamedPDF(file, filename) {
-    // we use an hard-coded fake path
-    // to not interfere with legit requests
-    const reg_path = "/caches/";
-    const url = reg_path + filename;
-    const store = await caches.open( "caches" );
-    await store.put( url, new Response( file ) );
-    console.log(await store.keys())
+	const inp = document.querySelector('input');
+	inp.onchange = e => {
+		const file = inp.files[0];
+		displayRenamedPDF(file, 'giga_test' + file.name);
+	};
 
-    window.open(url);
+	async function displayRenamedPDF(file, filename) {
+		// we use an hard-coded fake path
+		// to not interfere with legit requests
+		const reg_path = '/caches/';
+		const url = reg_path + filename;
+		const store = await caches.open('caches');
+		await store.put(url, new Response(file));
+		console.log(await store.keys());
 
-    return
-    const frame = document.createElement( "iframe" );
-    frame.width = 400
-    frame.height = 500;
-    const test = document.querySelector('.fedya_test');
-    test.append( frame );
-    frame.src = url;
-  }
+		window.open(url);
+
+		return;
+		const frame = document.createElement('iframe');
+		frame.width = 400;
+		frame.height = 500;
+		const test = document.querySelector('.fedya_test');
+		test.append(frame);
+		frame.src = url;
+	}
+
 // clear previously cached files
 //   (async () => {
 //     const store = await caches.open( "name-forcer" );
@@ -41,5 +55,5 @@ onMounted(async () => {
 //       store.delete( req );
 //     }
 //   })();
-})
+});
 </script>
