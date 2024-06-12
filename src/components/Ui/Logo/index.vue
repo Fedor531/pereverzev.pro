907b51323287d87574a597fb4fe6491332a515fe
Fedor Pereverzev <fedor531@yandex.ru>
2024-06-12 22:12:38 +0700

    refactor: logo

--- src/components/Ui/Logo/index.vue
+++ src/components/Ui/Logo/index.vue
@@ -1,65 +1,40 @@
 <template>
-  <NuxtLink class="ui-logo" to="/">
-    <div class="ui-logo__letter logo-letter">
-      F
-    </div>
-    <div class="logo-text">
-      <span class="logo-text__name">
-        Fedor
-      </span>
-      Pereverzev
-    </div>
-  </NuxtLink>
+	<NuxtLink
+		class="ui-logo"
+		to="/"
+	>
+		<img
+			class="ui-logo__image"
+			src="/logo.svg"
+			alt="logo">
+		<span>
+			pereverzev.pro
+		</span>
+	</NuxtLink>
 </template>
-
-<script lang="ts" setup>
-
-</script>
 
 <style lang="postcss">
 .ui-logo {
 	display: flex;
 	align-items: center;
-
-	&__letter {
-		margin-right: 8px;
-	}
-}
-
-.logo-letter {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
-	width: 44px;
-	height: 44px;
-
-	border-radius: 100px;
-	background-color: var(--color-blue);
-
-	font-size: var(--fs-4xl);
-	font-weight: 600;
-}
-
-.logo-text {
-
+	gap: 8px;
 	font-size: var(--fs-xl);
 
-	&__name {
-		font-weight: 600;
+	&__image {
+		width: 40px;
+		height: 40px;
 	}
 }
 
 @media all and (--screen-tablet) {
-	.logo-letter {
-		width: 32px;
-		height: 32px;
-
+	.ui-logo {
+		gap: 6px;
 		font-size: var(--fs-lg);
-	}
 
-	.logo-text {
-		font-size: var(--fs-lg);
+		&__image {
+			width: 36px;
+			height: 36px;
+		}
 	}
 }
 
