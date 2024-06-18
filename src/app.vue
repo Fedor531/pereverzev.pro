fdb3f8c2fa8014fe8158f069050a8a8281abea51
Fedor Pereverzev <fedor531@yandex.ru>
2024-06-18 12:33:02 +0700

    fix

--- src/app.vue
+++ src/app.vue
@@ -7,5 +7,16 @@
 </template>
 
 <script setup lang="ts">
-const { locale, } = useI18n()
+const { locale, setLocale } = useI18n()
+const event = useRequestEvent()
+
+let isEng = useState('is-eng', () => false);
+
+if (process.server && event.node.req.headers.host?.includes('en')) {
+	isEng.value = true
+}
+
+if (isEng.value) {
+	setLocale('en')
+}
 </script>
