9f21d5a6860a7a950f94f0a16b1421c6e4a023aa
Fedor Pereverzev <fedor531@yandex.ru>
2024-07-05 11:11:18 +0700

    fix

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -46,6 +46,28 @@ onMounted(() => {
 
 	window.addEventListener('resize', setVh);
 	setVh();
+
+
+	const script = document.createElement('script')
+	script.setAttribute('defer', '')
+	script.innerHTML = `
+		   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+			   m[i].l=1*new Date();
+			   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+			   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+			   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+			   ym(97752129, "init", {
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true,
+					webvisor:true
+			   });
+	`
+	document.body.appendChild(script)
+	const noscript = document.createElement('noscript')
+	noscript.innerHTML = `<div><img src="https://mc.yandex.ru/watch/97752129" style="position:absolute; left:-9999px;" alt="" /></div>`
+
 });
 </script>
 
