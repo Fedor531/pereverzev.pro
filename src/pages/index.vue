47d8fdacff29551cdca072a63b2eacef9dfb518b
Fedor Pereverzev <fedor531@yandex.ru>
2024-07-25 16:33:27 +0700

    fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
 	<div>
 		Главная
+
+		<NuxtLink to="/ssr/cache-swr">
+			/ssr/cache-swr
+		</NuxtLink>
+		<NuxtLink to="/ssr/cache-swr-date">
+			/ssr/cache-swr-date
+		</NuxtLink>
+		<NuxtLink to="/ssr/cache-swr-30-s-date">
+			/ssr/cache-swr-30-s-date
+		</NuxtLink>
 	</div>
 </template>
 
--- src/pages/ssr/cache-swr-30-s-date.vue
+++ src/pages/ssr/cache-swr-30-s-date.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script lang="ts" setup>
-const { data } = useAsyncData('cache-swr-date', async () => {
-	console.log('cache-swr-date ЗАПРОС ПОШЕЛ')
+const { data } = useAsyncData('cache-swr-30-s-date', async () => {
+	console.log('cache-swr-30-s-date ЗАПРОС ПОШЕЛ')
 
 	return new Promise((resolve, rej) => {
 		setTimeout(async() => {
-			console.log('setTimeout cache-swr-date ЗАПРОС ПРОИЗОШЕЛ')
+			console.log('setTimeout cache-swr-30-s-date ЗАПРОС ПРОИЗОШЕЛ')
 			const res = await $fetch('/api/data')
 			resolve(res)
-		}, 3000)
+		}, 0)
 	})
 })
 </script>
