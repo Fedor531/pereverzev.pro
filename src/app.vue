ff8e50b93c66b6a98d854670ba55182a6c5bb4fa
Fedor Pereverzev <fedor531@yandex.ru>
2024-07-29 13:22:37 +0700

    fix

--- src/app.vue
+++ src/app.vue
@@ -19,4 +19,31 @@ if (process.server && event.node.req.headers.host?.includes('en')) {
 if (isEng.value) {
 	setLocale('en')
 }
+
+const colorScheme = useCookie('color-scheme');
+
+let lightMedia = '(prefers-color-scheme: light)';
+let darkMedia = '(prefers-color-scheme: dark)';
+
+if (colorScheme.value) {
+	lightMedia = (colorScheme.value === 'light') ? 'all' : 'not all';
+	darkMedia = (colorScheme.value === 'dark') ? 'all' : 'not all';
+}
+
+useHead({
+	link: [
+		{
+			'data-theme': 'light',
+			rel: 'stylesheet',
+			media: lightMedia,
+			href: '/assets/styles/light.css'
+		},
+		{
+			'data-theme': 'dark',
+			rel: 'stylesheet',
+			media: darkMedia,
+			href: '/assets/styles/dark.css'
+		}
+	],
+})
 </script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -11,33 +11,6 @@
 <script lang="ts" setup>
 console.log('NUXT_ENVIRONMENT', useRuntimeConfig().public.NUXT_ENVIRONMENT)
 
-const colorScheme = useCookie('color-scheme');
-
-let lightMedia = '(prefers-color-scheme: light)';
-let darkMedia = '(prefers-color-scheme: dark)';
-
-if (colorScheme.value) {
-	lightMedia = (colorScheme.value === 'light') ? 'all' : 'not all';
-	darkMedia = (colorScheme.value === 'dark') ? 'all' : 'not all';
-}
-
-useHead({
-	link: [
-		{
-			'data-theme': 'light',
-			rel: 'stylesheet',
-			media: lightMedia,
-			href: '/assets/styles/light.css'
-		},
-		{
-			'data-theme': 'dark',
-			rel: 'stylesheet',
-			media: darkMedia,
-			href: '/assets/styles/dark.css'
-		}
-	],
-})
-
 const initYM = () => {
 	if (typeof window.ym === 'function') {
 		return
