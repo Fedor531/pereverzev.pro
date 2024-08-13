4d500d3f284c20e8c7613a2402ca4973e4fb4e6a
Fedor Pereverzev <fedor531@yandex.ru>
2024-08-13 11:41:42 +0700

    fix

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 cd /var/www/pereverzev.pro/nuxt
 sudo docker-compose down
-sudo docker rmi -f $(docker images -aq)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
 sudo docker-compose up -d
